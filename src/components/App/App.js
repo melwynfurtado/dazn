bebfcbeafc6be7f8db5b5f7d8ef274c2cfb2b583
Melwyn Furtado <melwynfurtado@gmail.com>
2018-11-20 01:31:44 +0000

    wire up services into component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import debounce from 'lodash/debounce'
+import theMovieDb from '../../services/theMovieDbService'
 import './App.scss'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      query: '',
+      results: []
+    }
+    this.handleOnChange = this.handleOnChange.bind(this)
+    this.getResultsDebounced = debounce(this.getResults, 500)
+  }
+
+  async getResults() {
+    const theMovieDbInstance = theMovieDb()
+    const defaultResponse = await theMovieDbInstance.getMulti({ query: this.state.query })
+    if (defaultResponse && defaultResponse.results) this.setState({ results: defaultResponse.results })
+  }
+
+  handleOnChange(e) {
+    this.setState({ query: e.target.value }, () => {
+      this.getResultsDebounced()
+    })
+  }
+
   render() {
-    return <h1>Hello World!</h1>
+    const { results, query } = this.state
+
+    return (
+      <div className="theMovieDb-search">
+      <h1>The Movie DB Search - DAZN</h1>
+      <input className="search-field" type="text" name="query" value={query} onChange={this.handleOnChange} />
+      <ul className="search-results">
+        {
+          results.map(m => <li key={m.id} className="item">{m.title || m.name}</li>)
+        }
+      </ul>
+      </div>
+    )
   }
 }
 
